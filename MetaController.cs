c23aefc97e473c32fb1b7360c98809dd44fc1b30
marti <103861195+martinagamon@users.noreply.github.com>
2022-11-14 22:48:18 -0300

    Create MetaController.cs

---
+++ MetaController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MetaController : MonoBehaviour
+{
+
+    [SerializeField] private Configuracion_General config;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            config.ganaste = true;
+        }
+    }
+}
